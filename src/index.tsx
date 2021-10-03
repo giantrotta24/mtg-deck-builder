62ba46c0aff7e10b51aec3c39d80d25477757fe3
Gian Trotta <giantrotta24@gmail.com>
2021-10-03 00:20:18 -0400

    add material ui and styled components

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import CssBaseline from '@mui/material/CssBaseline';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+    <CssBaseline />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
