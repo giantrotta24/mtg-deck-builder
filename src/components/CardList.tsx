4622bef295c7fe79899581b1892361c10b0b8a31
Gian Trotta <giantrotta24@gmail.com>
2021-10-06 23:54:35 -0400

    Add paper component to ListItem

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -3,13 +3,19 @@ import Box from '@mui/material/Box';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
+import Paper from '@mui/material/Paper';
 
 const CardList = ({ cards }: { cards: any[] }): JSX.Element => {
   return (
-    <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-      <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+    <Box>
+      <List>
         {cards.map(card => (
-          <ListItem key={card.id}>
+          <ListItem
+            component={Paper}
+            elevation={3}
+            sx={{ marginTop: '.5rem' }}
+            key={card.id}
+          >
             <ListItemText id={card.id} primary={card.name} />
           </ListItem>
         ))}
